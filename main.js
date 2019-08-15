5e7ff3d430ebb636ab3e1897b185a5162515f53d
Apple <apple@Apples-MacBook-Pro-59.local>
2019-08-15 11:20:59 -0400

    sticky notes done slider adjusted

--- main.js
+++ main.js
@@ -52,3 +52,11 @@ function showYear() {
     console.log(yea);
     section.append(`<p>${yea}</p>`);
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function(changeFace) {
+  output.innerHTML = this.value;
+}
