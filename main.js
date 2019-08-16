39a64d341608ebffe8dd23a58a3cb5dca4305c3e
Apple <apple@Apples-MacBook-Pro-59.local>
2019-08-16 08:52:53 -0400

    Slider isn't working, tips are

--- main.js
+++ main.js
@@ -1,56 +1,40 @@
-var extrableh = ["If people are doubting how far you can go, go so far that you can't hear them anymore. - Michele Ruiz", "Don't compare your life to others. There's no comparison between the sun and the moon, they shine when it's their time. -Unknown", "I BELIEVE IN YOU - Unknown"]
-var bleh = ["We are all in the gutter, but some of us are looking at the stars. - Oscar Wilde", "The only person you should try to be better than is the person you were yesterday. - Unknown", "A closed mind will give you an expected outcome, but an open mind will give you incredible possibilities. - Unknown", "There are far, far better things ahead than any we leave behind. - C.S. Lewis"]
-var meh = ["Make your life a masterpiece, imagine no limitations on what you can be, have, or do. - Brian Tracy", "Creativity takes courage. - Henri Matisse", "Expect problems and eat them for breakfast. - Alfred A. Montapert"]
-var yeh = ["Be somebody who makes everybody feel like a somebody. - Courtney Shields", "I find beauty in unusual things, like hanging your head out the window or sitting on a fir escape. - Scarlett Johansson", "If it's still on your mind, it;s worth taking the risk. - Unknown"]
-var extrayeh = ["Mind your own biscuits and life will be gravy. - Unknown", "Here comes a great day in 3 . . . 2 . . . 1 . . .GO", "No one is you and that is your power. - Unknown"]
-
 var tipday = ["Try to get everything done in the morning so you have a free evening to hang out with friends and watch Netflix.", "Write out a to-do list and a time you hope to finish each goal.", "Clean off your desk except for all the materials you will need. Put your phone in another room, and start working.", "Look up work music on Youtube and start at it. Sometimes all you need to do is begin and before you know it hours will have gone by.", "Work for 45 minutes and then take a break for 5-15 minutes, then repeat until all your work is done."]
-var tipweek = ["one", "two", "three"]
-var tipmonth = ["Set aside two days a month to meet with friends", "two", "three"]
-var tipyear = ["Make a mood board with pictures to represent what you want to get done (ex: a weight for exercise) and put it in a place that you will see everyday.", "Make a mind map at the beginning of the year with branches for spiritual health, physical health, religion, family, and hobby sectors."]
-
+var tipweek = ["Remind yourself to stay hydrated by drinking water daily.", "Focus more on yourself by staying offline one day per week.", "Try and finish one book in a week by reading a certain amount of pages each day.", "In the morning, try getting out of bed when your alarm goes off."]
+var tipmonth = ["Set aside two days a month to meet with friends.", "See if you can go one month without soda or coffee.", "Develop a workout routine to follow each day for a month.", "Try exploring new places in your town or city at least once per week."]
+var tipyear = ["Make a mood board with pictures to represent what you want to get done (ex: a weight for exercise)",  "Make a mind map at the beginning of the year with branches for spiritual health, physical health, religion, family, and hobby sectors.", "Over the course of a year, spend more time putting yourself first.", "Try doing something you've always put off. (ie: hobby, sport, goal).", "Connect with others by learning a new language."]
 var button = $('.day');
 var tip = tipday[Math.floor(Math.random() * tipday.length)];
 var section = $('.section')
-
 button.on("click", showDay);
 function showDay() {
     event.preventDefault();
     console.log(tip);
-    section.append(`<p>${tip}</p>`);
+     section.append(`${tip}</p>`);
 }
-
 var buttonweek = $('.week');
 var wek = tipweek[Math.floor(Math.random() * tipweek.length)];
 var section = $('.section')
-
 buttonweek.on("click", showWeek);
 function showWeek() {
-    event.preventDefault();
-    console.log(wek);
-    section.append(`<p>${wek}</p>`);
+   event.preventDefault();
+   console.log(wek);
+   section.append(`${wek}</p>`);
 }
-
 var buttonmonth = $('.month');
 var mon = tipmonth[Math.floor(Math.random() * tipmonth.length)];
 var section = $('.section')
-
 buttonmonth.on("click", showMonth);
 function showMonth() {
-    event.preventDefault();
-    console.log(mon);
-    section.append(`<p>${mon}</p>`);
+ event.preventDefault();
+ console.log(wek);
+ section.append(`${mon}</p>`);
 }
-
 var buttonyear = $('.year');
 var yea = tipyear[Math.floor(Math.random() * tipyear.length)];
 var section = $('.section')
-
 buttonyear.on("click", showYear);
 function showYear() {
-    event.preventDefault();
-    console.log(yea);
-    section.append(`<p>${yea}</p>`);
+event.preventDefault();
+console.log(yea);
+section.append(`${yea}</p>`);
 }
-
- 
--- second.js
+++ second.js
@@ -1,56 +1,31 @@
-//  var slider = $("slider");
-// output.innerHTML = slider.value;
-//
-//  slider.oninput = function() {
-//    output.innerHTML = this.value;
-//    if(ui.value == 50){
-//      console.log('Hi')
-//        $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/really-happy_r9hswo.png');
-//    }else if(ui.value == 100){
-//        $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/happy_k51drq.png');
-//    }else if(ui.value == 150){
-//        $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/average_t4kd0z.png');
-//   }else if(ui.value == 200){
-//       $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/sad_fm4jg7.png');
-//    }else(ui.value == 250){
-//        $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/really-mad_bsczhk.png');
-//    };
-// }
-//  });
-//  $("#mood").val(steps[$("#slider").slider("value")]);
-//  });
-//  }
-//
-//  $(function() {
-//      $("#slider").slider({
-//          value: 2,
-//          min: 0,
-//          max: 4,
-//          step: 1,
-//          slide: function(event, ui) {
-//             $("#mood").val(steps[ui.value]);
-//          },
-//          change: function(event, ui){
-//          if(ui.value == 0){
-//              $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/really-happy_r9hswo.png');
-//          }else if(ui.value == 1){
-//              $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/happy_k51drq.png');
-//          }else if(ui.value == 2){
-//              $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/average_t4kd0z.png');
-//          }else if(ui.value == 3){
-//             $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/sad_fm4jg7.png');
-//          }else if(ui.value == 4){
-//              $('#mood-icon').attr('src','https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/really-mad_bsczhk.png');
-//         };
-//        }
-//      });
-//      $("#mood").val(steps[$("#slider").slider("value")]);
-//  });
-
+var extrableh = ["If people are doubting how far you can go, go so far that you can't hear them anymore. - Michele Ruiz", "Don't compare your life to others. There's no comparison between the sun and the moon, they shine when it's their time. -Unknown", "I BELIEVE IN YOU - Unknown"]
+var bleh = ["We are all in the gutter, but some of us are looking at the stars. - Oscar Wilde", "The only person you should try to be better than is the person you were yesterday. - Unknown", "A closed mind will give you an expected outcome, but an open mind will give you incredible possibilities. - Unknown", "There are far, far better things ahead than any we leave behind. - C.S. Lewis"]
+var meh = ["Make your life a masterpiece, imagine no limitations on what you can be, have, or do. - Brian Tracy", "Creativity takes courage. - Henri Matisse", "Expect problems and eat them for breakfast. - Alfred A. Montapert"]
+var yeh = ["Be somebody who makes everybody feel like a somebody. - Courtney Shields", "I find beauty in unusual things, like hanging your head out the window or sitting on a fir escape. - Scarlett Johansson", "If it's still on your mind, it;s worth taking the risk. - Unknown"]
+var extrayeh = ["Mind your own biscuits and life will be gravy. - Unknown", "Here comes a great day in 3 . . . 2 . . . 1 . . .GO", "No one is you and that is your power. - Unknown"];
 var slide = document.getElementById('slide');
-    sliderDiv = document.getElementById("sliderAmount");
-
+var exb = extrableh[Math.floor(Math.random() * extrableh.length)];
+var ble = bleh[Math.floor(Math.random() * bleh.length)];
+var mah = meh[Math.floor(Math.random() * meh.length)];
+var ya = yeh[Math.floor(Math.random() * yeh.length)];
+var exy = extrayeh[Math.floor(Math.random() * extrayeh.length)];
+var sectionmd = $('.sectionmd')
+sliderDiv = document.getElementById("sliderAmount");
 slide.onchange = function() {
-    sliderDiv.innerHTML = this.value;
-    console.log('Hello');
-}​
+ if (this.value == 250) {
+ $('.face').attr('src', 'https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/really-happy_r9hswo.png(5 kB)');
+ (sectionmd).append(<p>${exy}</p>);
+}else if(this.value == 200){
+    $('.face').attr('src', 'https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/happy_k51drq.png(6 kB)');
+    (sectionmd).append(<p>${ya}</p>);
+}else if(this.value == 150){
+    $('.face').attr('src', 'https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/average_t4kd0z.png(4 kB)');
+    (sectionmd).append(<p>${mah}</p>);
+}else if(this.value == 100){
+    $('.face').attr('src', 'https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/sad_fm4jg7.png(5 kB)');
+    (sectionmd).append(<p>${ble}</p>);
+}else if(this.value == 50){
+    $('.face').attr('src', 'https://res.cloudinary.com/dv2ytxq4z/image/upload/v1489289355/really-mad_bsczhk.png(5 kB)');
+    (sectionmd).append(<p>${exb}</p>);
+}
+}
